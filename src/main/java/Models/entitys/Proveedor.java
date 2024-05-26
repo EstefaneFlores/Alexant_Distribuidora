157aa8264a11f184ecca48bfda6d47802fdaf6e3
esmeralda <chintariesmeralda7@gmail.com>
2024-05-26 15:31:14 -0400

    cambios ema

--- src/main/java/Models/entitys/Proveedor.java
+++ src/main/java/Models/entitys/Proveedor.java
@@ -1,5 +1,39 @@
 package Models.entitys;
 
-public class Proveedor {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table (name = "proveedor")
+public class Proveedor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_proveedor;
+   
+    @Column (name = "nombre_proveedor")
+    private String nombre_proveedor;
+
+    @Column (name = "telefono_proveedor")
+    private Integer telefono_proveedor;
+
+    @Column (name = "descripcion_proveedor")
+    private String descripcion_proveedor;
+
+    @Column (name = "estado proveedor")
+    private String estado_proveedor;
+
     
 }
--- src/main/java/Models/entitys/Recepcion_Producto.java
+++ src/main/java/Models/entitys/Recepcion_Producto.java
@@ -1,5 +1,49 @@
 package Models.entitys;
 
-public class Recepcion_Producto {
+import java.io.Serializable;
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table (name = "recepcion_producto")
+public class Recepcion_Producto implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+
+    @Column (name = "precio_producto")
+    private String precio_producto;
+
+    @Column (name = "cantidad_caja")
+    private Integer cantidad_caja;
+
+    @Column (name = "fecha_ingreso")
+    private Date fecha_ingreso;
+
+    @Column (name = "fecha_registro_producto")
+    private Date fecha_registro_producto;
+
+    @Column (name = "fecha_vencimento")
+    private Date fecha_vencimeinto;
+
+    @Column (name = "precio_venta")
+    private Float precio_venta;
+
+    @Column (name = "estado_recepcion_producto")
+    private String estado_recepcion_producto;
+
+
     
 }
